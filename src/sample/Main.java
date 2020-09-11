eef63baf5b1c110509c7db7c200e9a5ba6175de1
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-12 00:59:00 +0300

    createButton Creator method implemented

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -16,12 +15,16 @@ import java.util.*;
 
 public class Main extends Application {
 
-    Map<Type, Label> labels;
-    Map<Type, ObservableList<String>> observableLists;
-    Map<Type, ListView<String>> listViews;
+    private Map<Type, Label> labels;
+    private Map<Type, ObservableList<String>> observableLists;
+    private Map<Type, ListView<String>> listViews;
+
+    private Creator creator;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        creator = new Creator();
 
         GridPane rootNode = new GridPane();
         rootNode.setAlignment(Pos.CENTER);
@@ -29,22 +32,19 @@ public class Main extends Application {
         primaryStage.setTitle("Names");
         primaryStage.setScene(new Scene(rootNode, 500, 400));
 
-        labels = createLabels();
-        observableLists = createObservableLists();
-        listViews = createListViews();
+        labels = creator.createLabels();
+        observableLists = creator.createObservableLists();
+        listViews = creator.createListViews(observableLists);
         listViews.get(Type.SURNAME).getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-        Button fullNameEnterButton = new Button("Enter name");
+        Button fullNameEnterButton = creator.createButton("Enter name", 100, 25);
         fullNameEnterButton.setOnAction(event -> showInputTextDialog());
-        fullNameEnterButton.setMaxSize(100, 25);
 
-        Button processSelectedSurnamesButton = new Button("Process");
-        processSelectedSurnamesButton.setMaxSize(100, 25);
+        Button processSelectedSurnamesButton = creator.createButton("Process", 100, 25);
         processSelectedSurnamesButton.setOnAction(event -> processSelectedSurnames());
 
         rootNode.add(fullNameEnterButton, 0, 0);
         rootNode.add(processSelectedSurnamesButton, 3, 0);
-
         addLabelsToRootNode(rootNode, 1);
         addListViewsToRootNode(rootNode, 2);
 
@@ -63,32 +63,8 @@ public class Main extends Application {
         }
     }
 
-    public Map<Type, Label> createLabels() {
-        Map<Type, Label> labels = new HashMap<>();
-        for (Type type: Type.values()) {
-            labels.put(type, Helper.createLabel(type.toString(), 100, 50));
-        }
-        return labels;
-    }
-
-    public Map<Type, ObservableList<String>> createObservableLists() {
-        Map<Type, ObservableList<String>> observableLists = new HashMap<>();
-        for (Type type: Type.values()) {
-            observableLists.put(type, FXCollections.observableArrayList());
-        }
-        return observableLists;
-    }
-
-    public Map<Type, ListView<String>> createListViews() {
-        Map<Type, ListView<String>> listViews = new HashMap<>();
-        for (Type type: Type.values()) {
-            listViews.put(type, Helper.createListView(observableLists.get(type), 100, 300));
-        }
-        return listViews;
-    }
-
     private void showInputTextDialog() {
-        TextInputDialog textInputDialog = Helper.createTextInputDialog("Name", "Enter name", "Name: ");
+        TextInputDialog textInputDialog = creator.createTextInputDialog("Name", "Enter name", "Name: ");
         Optional<String> result = textInputDialog.showAndWait();
         result.ifPresent(this::processFullName);
     }
