1d1a77b4e18fbbcd3a84c47d887d3c9a92b42f79
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-12 19:44:06 +0300

    All is finished, MVC-model

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,27 +1,12 @@
 package code;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.util.*;
-
-
 public class Main extends Application {
-
-    private Map<Type, Label> labels;
-    private Map<Type, ObservableList<String>> observableLists;
-    private Map<Type, ListView<String>> listViews;
-
-    private Creator creator;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -33,59 +18,7 @@ public class Main extends Application {
         primaryStage.setTitle("Names");
         primaryStage.setWidth(500);
         primaryStage.setHeight(400);
-//
-//        labels = creator.createLabels();
-//        observableLists = creator.createObservableLists();
-//        listViews = creator.createListViews(observableLists);
-//        listViews.get(Type.SURNAME).getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-//
-//        Button fullNameEnterButton = creator.createButton("Enter name", 100, 25);
-//        fullNameEnterButton.setOnAction(event -> showInputTextDialog());
-//
-//        Button processSelectedSurnamesButton = creator.createButton("Process", 100, 25);
-//        processSelectedSurnamesButton.setOnAction(event -> processSelectedSurnames());
-
-        //rootNode.add(fullNameEnterButton, 0, 0);
-        //rootNode.add(processSelectedSurnamesButton, 3, 0);
-        //addLabelsToRootNode(rootNode, 1);
-       // addListViewsToRootNode(rootNode, 2);
 
         primaryStage.show();
-    }
-
-    public void addLabelsToRootNode(GridPane rootNode, int rowIndex) {
-        for (Type type: Type.values()) {
-            rootNode.add(labels.get(type), type.ordinal(), rowIndex);
-        }
-    }
-
-    public void addListViewsToRootNode(GridPane rootNode, int rowIndex) {
-        for (Type type: Type.values()) {
-            rootNode.add(listViews.get(type), type.ordinal(), rowIndex);
-        }
-    }
-
-    private void showInputTextDialog() {
-        TextInputDialog textInputDialog = creator.createTextInputDialog("Name", "Enter name", "Name: ");
-        Optional<String> result = textInputDialog.showAndWait();
-        result.ifPresent(this::processFullName);
-    }
-
-    private void processFullName(String fullName) {
-        String[] fullNameParts = fullName.split(" ");
-        observableLists.get(Type.NAME).add(fullNameParts[1]);
-        observableLists.get(Type.SURNAME).add(fullNameParts[0]);
-        observableLists.get(Type.FATHERLAND).add(fullNameParts[2]);
-    }
-
-    private void processSelectedSurnames() {
-        ArrayList<String> selectedSurnames = new ArrayList<>(listViews.get(Type.SURNAME).getSelectionModel().getSelectedItems());
-        selectedSurnames.sort(Comparator.naturalOrder());
-        observableLists.get(Type.SORTED_SURNAME).clear();
-        observableLists.get(Type.SORTED_SURNAME).addAll(selectedSurnames);
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
--- src/code/controllers/MainController.java
+++ src/code/controllers/MainController.java
@@ -1,4 +1,64 @@
 package code.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+
+
 public class MainController {
+
+    private ObservableList<String> names = FXCollections.observableArrayList();
+    private ObservableList<String> surnames = FXCollections.observableArrayList();
+    private ObservableList<String> fatherlands = FXCollections.observableArrayList();
+    private ObservableList<String> processedSurnames = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<String> surnameListView;
+
+    @FXML
+    private ListView<String> nameListView;
+
+    @FXML
+    private ListView<String> fatherlandListView;
+
+    @FXML
+    private ListView<String> processedListView;
+
+    public void initialize() {
+        surnameListView.setItems(surnames);
+        surnameListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        nameListView.setItems(names);
+        fatherlandListView.setItems(fatherlands);
+        processedListView.setItems(processedSurnames);
+    }
+
+    @FXML
+    private void showInputTextDialog() {
+        TextInputDialog textInputDialog = new TextInputDialog();
+        textInputDialog.setTitle("Name");
+        textInputDialog.setHeaderText("Enter name");
+        textInputDialog.setContentText("Name: ");
+        Optional<String> result = textInputDialog.showAndWait();
+        result.ifPresent(name -> {
+            String[] fullNameParts = name.split(" ");
+            surnames.add(fullNameParts[0]);
+            names.add(fullNameParts[1]);
+            fatherlands.add(fullNameParts[2]);
+        });
+    }
+
+    @FXML
+    private void processSelectedSurnames() {
+        ArrayList<String> selectedSurnames = new ArrayList<>(surnameListView.getSelectionModel().getSelectedItems());
+        selectedSurnames.sort(Comparator.naturalOrder());
+        processedSurnames.clear();
+        processedSurnames.addAll(selectedSurnames);
+    }
 }
